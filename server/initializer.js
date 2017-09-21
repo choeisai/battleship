220d272b14dd41b6b731210bd3ad7ec7061c02b9
Chalermrat Srinara <schalermrat@gmail.com>
2017-09-21 18:21:42 +0700

    feat(mongoose): Initialize mongoose

--- server/initializer.js
+++ server/initializer.js
@@ -2,5 +2,9 @@ module.exports = async (config) => {
   // Express
   const app = require('./initExpress')(config)
 
+  // Mongoose
+  const mongoose = await require('./initMongoose')(config)
+        .catch(err => debug.error(`MongoDB :`, err))
+
   return require('./initRoute')(config, app)
 }
